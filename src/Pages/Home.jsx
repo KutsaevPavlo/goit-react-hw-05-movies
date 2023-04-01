330ffa0fdb975c2c5ff08e514f9ae47966436bcd
KutsaevPavlo <p1ul@ukr.net>
2023-04-01 23:37:12 +0300

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,15 +8,9 @@ const Home = () => {
 
   return (
     <div>
-      {['movies-1', 'movies-2', 'movies-3', 'movies-4', 'movies-5'].map(
-        movies => {
-          return (
-            <Link key={movies} to={`${movies}`}>
-              {movies}
-            </Link>
-          );
-        }
-      )}
+      <ul>
+        <li>movies-1</li>
+      </ul>
     </div>
   );
 };
