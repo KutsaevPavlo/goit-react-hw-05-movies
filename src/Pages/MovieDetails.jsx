3027504c4275e6b152d4167b1533dc62f68ef07d
KutsaevPavlo <p1ul@ukr.net>
2023-04-01 23:36:14 +0300

    update

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,8 +1,23 @@
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
 
-  return <>MovieDetails: {movieId} </>;
+  return (
+    <>
+      <h1>MovieDetails: {movieId} </h1>
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews ">Reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </>
+  );
 };
 export default MovieDetails;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Movies from 'Pages/Movies';
 import MovieDetails from 'Pages/MovieDetails';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
+import { Cast } from './Cast';
+import { Reviews } from './Reviews';
 export const App = () => {
   return (
     <div>
@@ -10,7 +12,10 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </div>
